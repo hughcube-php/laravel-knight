618f9808d2a72a90fa01fee15f630e657d94d700
hugh.li <hugh.li@foxmail.com>
2021-06-05 22:16:46 +0800

    bug: fix php7.1

---
+++ tests/Routing/Action.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2021/6/4
+ * Time: 7:36 下午.
+ */
+
+namespace HughCube\Laravel\Knight\Tests\Routing;
+
+class Action extends \HughCube\Laravel\Knight\Routing\Action
+{
+    protected function rules()
+    {
+        return [
+            'uuid' => ['string']
+        ];
+    }
+
+    protected function action()
+    {
+        return $this->parameter()->all();
+    }
+}
