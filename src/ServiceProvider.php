0ed5821ee61cdaad5726a289dcd4c913812c2c36
hugh.li <hugh.li@foxmail.com>
2021-06-10 16:34:03 +0800

    feat: 添加ServiceProvider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2021/6/10
+ * Time: 4:29 下午
+ */
+
+namespace HughCube\Laravel\Knight;
+
+use HughCube\Laravel\Knight\Console\Commands\Config;
+use HughCube\Laravel\Knight\Console\Commands\Environment;
+use HughCube\Laravel\Knight\Console\Commands\PhpIniFile;
+use HughCube\Laravel\Knight\Console\Commands\RepeatTest;
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+
+class ServiceProvider extends IlluminateServiceProvider
+{
+    /**
+     * Boot the provider.
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the provider.
+     */
+    public function register()
+    {
+        $this->registerCommand();
+    }
+
+    protected function registerCommand()
+    {
+        $this->commands([
+            Config::class,
+            Environment::class,
+            PhpIniFile::class,
+            RepeatTest::class
+        ]);
+    }
+}
