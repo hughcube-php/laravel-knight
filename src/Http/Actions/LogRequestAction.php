bdea4b3348b70d6f3a6edc0bb3a367f4a8a5f1d5
hugh.li <hugh.li@foxmail.com>
2021-12-24 14:39:33 +0800

    feat: add actions

---
+++ src/Http/Actions/LogRequestAction.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2021/12/17
+ * Time: 21:36
+ */
+
+namespace HughCube\Laravel\Knight\Http\Actions;
+
+use Exception;
+use HughCube\Laravel\Knight\Routing\Action;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogRequestAction
+{
+    use Action;
+
+    /**
+     * @return Response
+     * @throws BindingResolutionException
+     * @throws Exception
+     */
+    public function action(): string
+    {
+        Log::info(sprintf(
+            'request: uri:%s, headers:%s, body:%s',
+            $this->getRequest()->getUri(),
+            json_encode($this->getRequest()->headers->all()),
+            $this->getRequest()->getContent()
+        ));
+
+        return 'success';
+    }
+}
---
+++ src/Http/Actions/PingAction.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2021/12/17
+ * Time: 21:36
+ */
+
+namespace HughCube\Laravel\Knight\Http\Actions;
+
+use HughCube\Laravel\Knight\Routing\Action;
+
+class PingAction
+{
+    use Action;
+
+    public function action(): string
+    {
+        return 'done';
+    }
+}
