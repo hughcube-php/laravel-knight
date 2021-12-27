55fbe2896a663fc1a8caaed1fdd81aba076c6fe6
hugh.li <hugh.li@foxmail.com>
2021-12-27 15:19:07 +0800

    feat: fix bug

--- src/OPcache/Jobs/WatchFilesJob.php
+++ src/OPcache/Jobs/WatchFilesJob.php
@@ -6,7 +6,8 @@ use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\RequestOptions;
 use HughCube\Laravel\Knight\Support\HttpClient;
 use HughCube\PUrl\Url;
-use Psr\Http\Message\ResponseInterface;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
 
 class WatchFilesJob extends \HughCube\Laravel\Knight\Queue\Job
 {
@@ -22,12 +23,21 @@ class WatchFilesJob extends \HughCube\Laravel\Knight\Queue\Job
     }
 
     /**
-     * @return ResponseInterface
+     * @return mixed
      * @throws GuzzleException
      */
-    protected function getResponse(): ResponseInterface
+    protected function getResponse(): mixed
     {
         $url = Url::instance(route('knight_opcache_scripts'));
-        return $this->getHttpClient()->get($url, [RequestOptions::TIMEOUT => 10.0]);
+        $response = $this->getHttpClient()->get($url, [RequestOptions::TIMEOUT => 10.0]);
+
+        $results = json_decode($response->getBody()->getContents(), true);
+
+        /** debug log */
+        $count = Arr::get($results, 'data.count');
+        $message = 'watch OPcache files, count: %s, status: %s, url: %s';
+        Log::debug(sprintf($message, $count, $response->getStatusCode(), $url->toString()));
+
+        return $results;
     }
 }
