bbc39d2e70281aa00831217330d2e5e6ba3b51ad
hugh.li <hugh.li@foxmail.com>
2021-12-28 19:25:21 +0800

    feat: add ScheduleJob

---
+++ src/Queue/Jobs/ScheduleJob.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace HughCube\Laravel\Knight\Queue\Jobs;
+
+use Carbon\Carbon;
+use Cron\CronExpression;
+use HughCube\Laravel\Knight\Queue\Job;
+use HughCube\Laravel\Knight\Support\MultipleHandler;
+use Illuminate\Contracts\Bus\Dispatcher;
+use Illuminate\Support\Facades\Date;
+use Throwable;
+
+class ScheduleJob extends Job
+{
+    use MultipleHandler;
+
+    protected ?Carbon $startData = null;
+
+    /**
+     * @throws Throwable
+     */
+    protected function action(): void
+    {
+        $this->startData = Carbon::now();
+        $this->triggerHandlers(true);
+    }
+
+    protected function getDelays(): int
+    {
+        return $this->startData->diffInRealMilliseconds(Carbon::now());
+    }
+
+    /**
+     * 判断是否可以运行
+     *
+     * @param $expression
+     * @return bool
+     */
+    protected function isDue($expression): bool
+    {
+        $date = Date::now();
+
+        return (new CronExpression($expression))->isDue($date->toDateTimeString());
+    }
+
+    /**
+     * push任务
+     *
+     * @param  Job  $job
+     * @return void
+     */
+    protected function pushJob(Job $job)
+    {
+        $id = app(Dispatcher::class)->dispatch($job);
+        $this->info(sprintf('job: %s, id:%s, delays:%sms', $this->getName($job), $id, $this->getDelays()));
+    }
+}
