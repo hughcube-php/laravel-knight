d889ece12cf21d102623257e191ca77d90b36dfd
hugh.li <hugh.li@foxmail.com>
2022-02-28 21:20:45 +0800

    feat: tag deprecated

---
+++ src/Octane/Octane.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2021/12/17
+ * Time: 14:56.
+ */
+
+namespace HughCube\Laravel\Knight\Octane;
+
+/**
+ * @deprecated Will be removed in a future version.
+ */
+class Octane
+{
+}
---
+++ src/Support/ParameterBagBak.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2022/2/17
+ * Time: 22:21.
+ */
+
+namespace HughCube\Laravel\Knight\Support;
+
+/**
+ * @deprecated Will be removed in a future version.
+ */
+class ParameterBagBak
+{
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function all(/*string $key = null*/)
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function keys()
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function replace(array $parameters = [])
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function add(array $parameters = [])
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function get(string $key, $default = null)
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function set(string $key, $value)
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function has(string $key)
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function remove(string $key)
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function getAlpha(string $key, string $default = '')
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function getAlnum(string $key, string $default = '')
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function getDigits(string $key, string $default = '')
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function getInt(string $key, int $default = 0)
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function getBoolean(string $key, bool $default = false)
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function filter(string $key, $default = null, int $filter = \FILTER_DEFAULT, $options = [])
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function getIterator()
+    {
+    }
+
+    /**
+     * @deprecated Will be removed in a future version.
+     */
+    public function count(): int
+    {
+        return 0;
+    }
+}
