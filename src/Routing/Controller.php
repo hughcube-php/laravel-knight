7c9f12f4b6c916d49c833b8e192a051a35282662
hugh.li <hugh.li@foxmail.com>
2022-02-28 17:30:18 +0800

    feat: Support php7.2

---
+++ src/Routing/Controller.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2022/2/28
+ * Time: 17:18
+ */
+
+namespace HughCube\Laravel\Knight\Routing;
+
+use HughCube\Laravel\Knight\Routing\Action as BaseAction;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class Controller extends \Illuminate\Routing\Controller
+{
+    use BaseAction;
+    use DispatchesJobs;
+    use AuthorizesRequests;
+
+    protected function action()
+    {
+        throw new NotFoundHttpException();
+    }
+}
