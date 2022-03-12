03190b2f254fbce5eefbbbe2fdd8a6081624a3d0
hugh.li <hugh.li@foxmail.com>
2022-03-12 22:39:34 +0800

    feat: add KnightModelTrait

---
+++ src/Database/Eloquent/Traits/KnightModelTrait.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace HughCube\Laravel\Knight\Database\Eloquent\Traits;
+
+/**
+ * Some code generators are not compatible with the Model class
+ */
+trait KnightModelTrait
+{
+    use Model;
+}
