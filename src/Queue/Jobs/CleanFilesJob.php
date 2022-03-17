b7dca683ae5d98bd6f0f0768737a325b6c7ce077
hugh.li <hugh.li@foxmail.com>
2022-03-17 19:49:07 +0800

    feat: add CleanFilesJob

---
+++ src/Queue/Jobs/CleanFilesJob.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace HughCube\Laravel\Knight\Queue\Jobs;
+
+use HughCube\Laravel\Knight\Queue\Job;
+use HughCube\Laravel\Knight\Support\Carbon;
+use Illuminate\Support\Facades\File;
+use Symfony\Component\Finder\Finder;
+
+class CleanFilesJob extends Job
+{
+    /**
+     * @return array
+     */
+    protected function rules(): array
+    {
+        return [
+            'items' => ['required', 'array', 'min:1'],
+
+            'items.*.dir' => ['required'],
+            'items.*.pattern' => ['nullable'],
+            'items.*.max_days' => ['required', 'integer', 'min:0'],
+        ];
+    }
+
+    protected function action(): void
+    {
+        foreach ($this->p('items', []) as $item) {
+            $this->cleanFiles($item);
+        }
+    }
+
+    protected function cleanFiles($job)
+    {
+        $dirs = $job['dir'];
+        $patterns = $job['pattern'] ?? '*';
+        $maxDays = $job['max_days'] ?: 30;
+
+        $files = Finder::create()->in($dirs)->name($patterns)->files();
+
+        $count = 0;
+        foreach ($files as $file) {
+            $mDate = Carbon::createFromTimestamp($file->getMTime());
+            if (!$mDate->addDays($maxDays)->isPast()) {
+                continue;
+            }
+
+            $count++;
+            File::delete($file);
+        }
+
+        $this->info(sprintf(
+            'Delete %s files in directory "%s" that match "%s".',
+            $count,
+            implode(',', $dirs),
+            implode(',', $patterns)
+        ));
+    }
+}
