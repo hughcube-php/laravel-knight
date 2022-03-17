06bc5d91e1bf01f71876992b7d6e8d04f9ce43ca
hugh.li <hugh.li@foxmail.com>
2022-03-17 19:46:46 +0800

    feat: add CleanFilesJob

--- tests/Queue/Jobs/CleanFilesJobTest.php
+++ tests/Queue/Jobs/CleanFilesJobTest.php
@@ -3,12 +3,13 @@
 namespace HughCube\Laravel\Knight\Tests\Queue\Jobs;
 
 use HughCube\Laravel\Knight\Queue\Jobs\AutoCleanFileJob;
+use HughCube\Laravel\Knight\Queue\Jobs\CleanFilesJob;
 use HughCube\Laravel\Knight\Tests\TestCase;
 
-class AutoCleanFileJobTest extends TestCase
+class CleanFilesJobTest extends TestCase
 {
     public function testRun()
     {
-        $this->assertJob(AutoCleanFileJob::new());
+        $this->assertJob(CleanFilesJob::new());
     }
 }
