45d467e1d2de005b50ef21cab9d7a123c0891a02
hugh.li <hugh.li@foxmail.com>
2022-03-19 17:59:37 +0800

    feat: add GetId

---
+++ src/Contracts/Support/GetId.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2022/3/19
+ * Time: 17:57
+ */
+
+namespace HughCube\Laravel\Knight\Contracts\Support;
+
+interface GetId
+{
+    /**
+     * @return mixed
+     */
+    public function getId();
+}
