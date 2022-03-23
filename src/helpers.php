60ab9657da4a90d578f08f663995efe8a7e4ba13
hugh.li <hugh.li@foxmail.com>
2022-03-23 13:16:15 +0800

    feat: add helpers.php

---
+++ src/helpers.php
@@ -0,0 +1,21 @@
+<?php
+
+if (!function_exists('log_path')) {
+
+    /**
+     * Get the path to the log folder.
+     *
+     * @param  string  $path
+     * @return string
+     */
+    function log_path(string $path = ''): string
+    {
+        $logPath = config('app.log_path', storage_path('logs'));
+
+        if (empty($path)) {
+            return $logPath;
+        }
+
+        return rtrim($logPath, '/').DIRECTORY_SEPARATOR.ltrim($path, '/');
+    }
+}
