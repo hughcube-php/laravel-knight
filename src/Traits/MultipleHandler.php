a679831ff01e97621896a332a3e7e59492c9438a
hugh.li <hugh.li@foxmail.com>
2022-03-30 17:54:52 +0800

    feat: add MultipleHandler

--- src/Traits/MultipleHandler.php
+++ src/Traits/MultipleHandler.php
@@ -10,6 +10,8 @@ namespace HughCube\Laravel\Knight\Traits;
 
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Support\Str;
+use ReflectionClass;
+use ReflectionMethod;
 use Throwable;
 
 trait MultipleHandler
@@ -20,12 +22,12 @@ trait MultipleHandler
     }
 
     /**
-     * @param bool $tryException
-     * @param bool $logException
-     *
-     * @throws Throwable
+     * @param  bool  $tryException
+     * @param  bool  $logException
      *
      * @return mixed
+     * @throws Throwable
+     *
      */
     protected function triggerHandlers(bool $tryException = false, bool $logException = true)
     {
@@ -64,20 +66,35 @@ trait MultipleHandler
     protected function getHandlers(): array
     {
         $handlers = [];
-        foreach (get_class_methods($this) as $method) {
-            if (!Str::contains(strtolower($method), 'handler')) {
+
+        $reflection = new ReflectionClass($this);
+        foreach ($reflection->getMethods() as $method) {
+            $handler = $this->parseHandlerMethod($method);
+            if (!is_array($handler) || !isset($handler[0], $handler[1])) {
                 continue;
             }
-
-            $sort = Str::afterLast(strtolower($method), 'handler');
-            if ('' !== $sort && !is_numeric($sort)) {
-                continue;
-            }
-
-            $handlers[$method] = intval($sort);
+            $handlers[$handler[0]] = $handler[1];
         }
         asort($handlers);
 
         return array_keys($handlers);
+    }
+
+    /**
+     * @param  ReflectionMethod  $method
+     * @return null|array
+     */
+    protected function parseHandlerMethod(ReflectionMethod $method): ?array
+    {
+        if (!Str::is('*handler*', strtolower($method->name))) {
+            return null;
+        }
+
+        $sort = Str::afterLast(strtolower($method->name), 'handler');
+        if ('' !== $sort && !ctype_digit($sort)) {
+            return null;
+        }
+
+        return [$method->name, intval(($sort ?: 0))];
     }
 }
---
+++ tests/Traits/MultipleHandlerTest.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2022/3/30
+ * Time: 17:36
+ */
+
+namespace HughCube\Laravel\Knight\Tests\Traits;
+
+use HughCube\Laravel\Knight\Tests\TestCase;
+use HughCube\Laravel\Knight\Traits\MultipleHandler;
+
+class MultipleHandlerTest extends TestCase
+{
+    /**
+     * @throws \ReflectionException
+     */
+    public function testMultipleHandler()
+    {
+        $job = new class() {
+            use MultipleHandler;
+
+            public $doneHandlers = [];
+
+
+            protected function aHandler10000()
+            {
+                $this->doneHandlers[] = __FUNCTION__;
+            }
+
+            protected function bHandler()
+            {
+                $this->doneHandlers[] = __FUNCTION__;
+            }
+
+            protected function cHandler100()
+            {
+                $this->doneHandlers[] = __FUNCTION__;
+            }
+
+            protected function dHandlerA()
+            {
+                $this->doneHandlers[] = __FUNCTION__;
+            }
+        };
+
+        $this->assertSame($this->callMethod($job, 'getHandlers'), ['bHandler', 'cHandler100', 'aHandler10000']);
+
+        $this->assertEmpty($this->getProperty($job, 'doneHandlers'));
+        $this->callMethod($job, 'triggerHandlers');
+        $this->assertSame($this->getProperty($job, 'doneHandlers'), ['bHandler', 'cHandler100', 'aHandler10000']);
+    }
+}
