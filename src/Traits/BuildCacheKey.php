ba6583af92e88f62f836ed15aa117eb5aa3bd9fc
hugh.li <hugh.li@foxmail.com>
2022-04-03 22:22:14 +0800

    feat: BuildCacheKey

---
+++ src/Traits/BuildCacheKey.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2022/4/3
+ * Time: 22:21
+ */
+
+namespace HughCube\Laravel\Knight\Traits;
+
+trait BuildCacheKey
+{
+    protected function buildCacheKey($data, $length = 32): string
+    {
+        $string = serialize($data);
+
+        $crc32 = base_convert(abs(crc32($string)), 10, 36);
+        $hash = sprintf('%s%s', $crc32, md5($string));
+
+        return substr($hash, 0, $length);
+    }
+}
