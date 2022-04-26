c6da73fb70067e45e5aeba4b4aa8f812ddafe1e6
hugh.li <hugh.li@foxmail.com>
2022-04-26 11:20:06 +0800

    feat: add TrustIps

---
+++ src/Http/Middleware/TrustIps.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2022/4/26
+ * Time: 10:47
+ */
+
+namespace HughCube\Laravel\Knight\Http\Middleware;
+
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use Symfony\Component\HttpFoundation\IpUtils;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrustIps
+{
+    /**
+     * The application instance.
+     *
+     * @var Container
+     */
+    protected $app;
+
+    /**
+     * Create a new middleware instance.
+     *
+     * @param  Container  $app
+     *
+     * @return void
+     */
+    public function __construct(Container $app)
+    {
+        $this->app = $app;
+    }
+
+    /**
+     * @param  Request  $request
+     * @param  callable  $next
+     * @param  null|string  $ips
+     * @return Response
+     * @throws AuthorizationException
+     */
+    public function handle(Request $request, callable $next, string $ips = null): Response
+    {
+        if (!$this->isTrustIp($request->ip(), $ips)) {
+            throw new AuthorizationException('An untrusted IP address!');
+        }
+
+        return $next($request);
+    }
+
+    protected function isTrustIp(string $requestIp, string $ips = null): bool
+    {
+        if (empty($ips) || '*' === $ips || '**' === $ips) {
+            return true;
+        }
+
+        if (empty($requestIp)) {
+            return true;
+        }
+
+        $ips = array_values(array_unique(array_filter(Arr::wrap(explode(',', $ips)))));
+        if (empty($ips)) {
+            return true;
+        }
+
+        if (IpUtils::checkIp($requestIp, $ips)) {
+            return true;
+        }
+
+        return false;
+    }
+}
