a6381eeb3f3b8757694b9da94c74db4c2e8f19a5
hugh.li <hugh.li@foxmail.com>
2022-06-29 16:56:23 +0800

    feat: 添加一些中间件

---
+++ src/Http/Middleware/CheckAbilities.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2022/6/29
+ * Time: 16:50
+ */
+
+namespace HughCube\Laravel\Knight\Http\Middleware;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAbilities extends \Laravel\Sanctum\Http\Middleware\CheckAbilities
+{
+    public function handle($request, $next, ...$abilities): Response
+    {
+        if (!$request->user() || !$request->user()->currentAccessToken()) {
+            return $next($request);
+        }
+        return parent::handle($request, $next, $abilities);
+    }
+}
---
+++ src/Http/Middleware/CheckForAnyAbility.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2022/6/29
+ * Time: 16:54
+ */
+
+namespace HughCube\Laravel\Knight\Http\Middleware;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckForAnyAbility extends \Laravel\Sanctum\Http\Middleware\CheckForAnyAbility
+{
+    public function handle($request, $next, ...$abilities): Response
+    {
+        if (!$request->user() || !$request->user()->currentAccessToken()) {
+            return $next($request);
+        }
+        return parent::handle($request, $next, $abilities);
+    }
+}
