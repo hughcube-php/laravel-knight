bfa81554ad852a1372a0134d9489637e38768f0c
hugh.li <hugh.li@foxmail.com>
2022-07-22 21:34:04 +0800

    feat: add Json

--- src/Support/Json.php
+++ src/Support/Json.php
@@ -13,11 +13,18 @@ class Json
     public static function decode($json, bool $associative = false, int $depth = 512, int $flags = 0)
     {
         $meta = json_decode($json, $associative, $depth, $flags);
-
-        if (null === $meta) {
-            return null;
-        }
-
         return JSON_ERROR_NONE === json_last_error() ? $meta : null;
+    }
+
+    public static function decodeArray($json, int $depth = 512, int $flags = 0): ?array
+    {
+        $meta = static::decode($json, true, $depth, $flags);
+        return is_array($meta) ? $meta : null;
+    }
+
+    public static function decodeObject($json, int $depth = 512, int $flags = 0): ?object
+    {
+        $meta = static::decode($json, false, $depth, $flags);
+        return is_object($meta) ? $meta : null;
     }
 }
