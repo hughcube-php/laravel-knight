4aa1996d810dd83053d36613f7a2eacc1f7baa43
hugh.li <hugh.li@foxmail.com>
2022-09-08 11:47:49 +0800

    feat: 手机号码兼容166*

--- src/Routing/GetMobile.php
+++ src/Routing/GetMobile.php
@@ -44,7 +44,7 @@ trait GetMobile
             return false;
         }
 
-        $pattern = '/^(13[0-9]|14[0-9]|15[0-9]|17[0-9]|18[0-9]|19[0-9])\d{8}$/';
+        $pattern = '/^(13[0-9]|14[0-9]|15[0-9]|16[0-9]|17[0-9]|18[0-9]|19[0-9])\d{8}$/';
 
         return false != preg_match($pattern, $mobile);
     }
