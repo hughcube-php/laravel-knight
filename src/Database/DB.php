1f2c32bc611d763da1c048c2e6f46c6a790f9698
hugh.li <hugh.li@foxmail.com>
2022-11-03 13:35:58 +0800

    feat: add DB Facade

---
+++ src/Database/DB.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2022/11/3
+ * Time: 13:33
+ */
+
+namespace HughCube\Laravel\Knight\Database;
+
+use Illuminate\Database\QueryException;
+use Throwable;
+
+class DB extends \Illuminate\Support\Facades\DB
+{
+    /**
+     * @throws Throwable
+     */
+    public static function retryOnQueryException(callable $callable, $count = 2, $microseconds = null)
+    {
+        $results = null;
+        $exception = null;
+
+        for ($i = 1; $i <= $count; $i++) {
+            try {
+                $results = $callable();
+            } catch (QueryException $exception) {
+                usleep(($microseconds ?: random_int(100000, 1000000)));
+            }
+        }
+
+        if ($exception instanceof Throwable) {
+            throw $exception;
+        }
+
+        return $results;
+    }
+}
