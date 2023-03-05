fbfed2c7ca718e7adeac78710b818dccc0e55ca6
hugh.li <hugh.li@foxmail.com>
2023-03-05 22:44:31 +0800

    feat: 添加laravel多版本的测试

---
+++ tests/Console/Commands/KR.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2022/2/26
+ * Time: 20:23.
+ */
+
+namespace HughCube\Laravel\Knight\Tests\Console\Commands;
+
+/**
+ * @
+ */
+class KR
+{
+    public function run($i)
+    {
+        return $i;
+    }
+}
