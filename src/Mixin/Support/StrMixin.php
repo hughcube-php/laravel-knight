89019f87094aa76f4402b87befe23adb450b7bee
hugh.li <hugh.li@foxmail.com>
2023-03-21 10:35:06 +0800

    bug: fix phpcs

--- src/Mixin/Support/StrMixin.php
+++ src/Mixin/Support/StrMixin.php
@@ -407,11 +407,11 @@ class StrMixin
             } else {
                 // @codingStandardsIgnoreStart
                 $re['utf-8'] = "/[\x01-\x7f]|[\xc2-\xdf][\x80-\xbf]|[\xe0-\xef][\x80-\xbf]{2}|[\xf0-\xff][\x80-\xbf]{3}/";
-                // @codingStandardsIgnoreEnd
-
                 $re['gb2312'] = "/[\x01-\x7f]|[\xb0-\xf7][\xa0-\xfe]/";
                 $re['gbk'] = "/[\x01-\x7f]|[\x81-\xfe][\x40-\xfe]/";
                 $re['big5'] = "/[\x01-\x7f]|[\x81-\xfe]([\x40-\x7e]|\xa1-\xfe])/";
+                // @codingStandardsIgnoreEnd
+
                 preg_match_all($re[$charset], $str, $match);
                 $slice = join("", array_slice($match[0], $start, $length));
             }
