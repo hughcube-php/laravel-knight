b503cd63eeac137cab7429791fca237fb0468876
hugh.li <hugh.li@foxmail.com>
2023-04-29 12:08:33 +0800

    Apply fixes from StyleCI (#102)
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- src/Sanctum/PersonalAccessToken.php
+++ src/Sanctum/PersonalAccessToken.php
@@ -10,16 +10,16 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 /**
  * Class PersonalAccessToken.
  *
- * @property int $id
- * @property string $tokenable_type
- * @property int $tokenable_id
- * @property string $name
- * @property string $token
+ * @property int               $id
+ * @property string            $tokenable_type
+ * @property int               $tokenable_id
+ * @property string            $name
+ * @property string            $token
  * @property string|array|null $abilities
- * @property Carbon|null $last_used_at
- * @property Carbon|null $expires_at
- * @property Carbon|null $created_at
- * @property Carbon|null $updated_at
+ * @property Carbon|null       $last_used_at
+ * @property Carbon|null       $expires_at
+ * @property Carbon|null       $created_at
+ * @property Carbon|null       $updated_at
  *
  * @method static static findById($id)
  */
