7a9301ed65b4278f8bbfda883c922d7019588b30
hugh.li <hugh.li@foxmail.com>
2023-05-01 22:34:18 +0800

    bug: 移除debug信息

--- src/Traits/Logger.php
+++ src/Traits/Logger.php
@@ -117,7 +117,6 @@ trait Logger
      */
     public function info(string $message, array $context = []): void
     {
-        echo $message, PHP_EOL;
         $this->log(LogLevel::INFO, $message, $context);
     }
 
