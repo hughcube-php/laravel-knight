8c2bae0cd0e8f7555c50dab5443e224ca5ef0dbc
hugh.li <hugh.li@foxmail.com>
2023-05-21 15:46:10 +0800

    feat: token刷新增加自定义代理

--- src/Queue/Jobs/RefreshWeChatMiniAppAccessTokensJob.php
+++ src/Queue/Jobs/RefreshWeChatMiniAppAccessTokensJob.php
@@ -2,11 +2,22 @@
 
 namespace HughCube\Laravel\Knight\Queue\Jobs;
 
+use EasyWeChat\MiniApp\Application as MiniApp;
 use HughCube\Laravel\Knight\Queue\Job;
 use HughCube\Laravel\WeChat\WeChat;
 
 class RefreshWeChatMiniAppAccessTokensJob extends Job
 {
+    /**
+     * @return array
+     */
+    protected function rules(): array
+    {
+        return [
+            'proxy' => ['nullable', 'string'],
+        ];
+    }
+
     protected function action(): void
     {
         $accounts = $this->getContainerConfig()->get('easywechat.mini_app', []);
@@ -18,8 +29,25 @@ class RefreshWeChatMiniAppAccessTokensJob extends Job
                 continue;
             }
 
+            $app = $this->resetHttpClient($app);
+
             $token = $app->getAccessToken()->refresh();
+
             $this->info(sprintf('刷新小程序(%s)token: %s', $appid, $token));
         }
+    }
+
+    protected function resetHttpClient(MiniApp $app): MiniApp
+    {
+        $proxy = $this->p('proxy');
+        if (empty($proxy)) {
+            return $app;
+        }
+
+        $httpClient = $app->getHttpClient()->withOptions([
+            'proxy' => $this->p('proxy'),
+        ]);
+
+        return $app->setHttpClient($httpClient);
     }
 }
--- src/Queue/Jobs/RefreshWeChatOfficialAccountAccessTokensJob.php
+++ src/Queue/Jobs/RefreshWeChatOfficialAccountAccessTokensJob.php
@@ -2,11 +2,22 @@
 
 namespace HughCube\Laravel\Knight\Queue\Jobs;
 
+use EasyWeChat\OfficialAccount\Application as OfficialAccount;
 use HughCube\Laravel\Knight\Queue\Job;
 use HughCube\Laravel\WeChat\WeChat;
 
 class RefreshWeChatOfficialAccountAccessTokensJob extends Job
 {
+    /**
+     * @return array
+     */
+    protected function rules(): array
+    {
+        return [
+            'proxy' => ['nullable', 'string'],
+        ];
+    }
+
     protected function action(): void
     {
         $accounts = $this->getContainerConfig()->get('easywechat.official_account', []);
@@ -18,9 +29,25 @@ class RefreshWeChatOfficialAccountAccessTokensJob extends Job
                 continue;
             }
 
+            $app = $this->resetHttpClient($app);
+
             $token = $app->getAccessToken()->refresh();
 
             $this->info(sprintf('刷新公众号(%s)token: %s', $appid, $token));
         }
+    }
+
+    protected function resetHttpClient(OfficialAccount $app): OfficialAccount
+    {
+        $proxy = $this->p('proxy');
+        if (empty($proxy)) {
+            return $app;
+        }
+
+        $httpClient = $app->getHttpClient()->withOptions([
+            'proxy' => $this->p('proxy'),
+        ]);
+
+        return $app->setHttpClient($httpClient);
     }
 }
