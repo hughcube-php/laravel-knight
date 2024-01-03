b87347562c4bc8950651c1fd2d34401f2c2e3093
hugh.li <hugh.li@foxmail.com>
2024-01-03 23:08:17 +0800

    feat: 性能优化

--- src/Routing/Controller.php
+++ src/Routing/Controller.php
@@ -10,14 +10,10 @@ namespace HughCube\Laravel\Knight\Routing;
 
 use HughCube\Laravel\Knight\Exceptions\NotExtendedHttpException;
 use HughCube\Laravel\Knight\Routing\Action as BaseAction;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 
-class Controller extends \Illuminate\Routing\Controller
+class Controller
 {
     use BaseAction;
-    use DispatchesJobs;
-    use AuthorizesRequests;
 
     protected function action()
     {
