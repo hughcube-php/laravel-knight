a981be05e50e4446e588473a76f0fc4d7f3a9599
hugh.li <hugh.li@foxmail.com>
2024-01-16 12:00:11 +0800

    feat: 添加job test

---
+++ src/Mixin/Database/Eloquent/CollectionMixin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2022/8/3
+ * Time: 15:03.
+ */
+
+namespace HughCube\Laravel\Knight\Mixin\Database\Eloquent;
+
+use Illuminate\Database\Eloquent\Collection;
+
+/**
+ * @mixin Collection
+ */
+class CollectionMixin
+{
+}
---
+++ src/Testing/Traits/TestJobCase.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2024/1/16
+ * Time: 11:49
+ */
+
+namespace HughCube\Laravel\Knight\Testing\Traits;
+
+use Illuminate\Foundation\Testing\TestCase;
+
+/**
+ * @mixin TestCase
+ */
+trait TestJobCase
+{
+    abstract public function getJob(): object;
+
+    public function testAction()
+    {
+        $job = $this->getJob();
+
+        if (method_exists($job, 'setLogChannel')) {
+            $job->setLogChannel('stdout');
+        }
+
+        $job->handle();
+
+        $this->assertTrue(true);
+    }
+}
