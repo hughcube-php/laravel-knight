3daff71889d0561f2640ca49a40cbdf27ec61157
hugh.li <hugh.li@foxmail.com>
2024-01-23 23:22:27 +0800

    Apply fixes from StyleCI (#128)
    Co-authored-by: StyleCI Bot <bot@styleci.io>

--- tests/Queue/Jobs/ScheduleJobTest.php
+++ tests/Queue/Jobs/ScheduleJobTest.php
@@ -5,7 +5,6 @@ namespace HughCube\Laravel\Knight\Tests\Queue\Jobs;
 use HughCube\Laravel\Knight\Queue\Jobs\CleanFilesJob;
 use HughCube\Laravel\Knight\Queue\Jobs\ScheduleJob;
 use HughCube\Laravel\Knight\Tests\TestCase;
-use Illuminate\Support\Str;
 
 class ScheduleJobTest extends TestCase
 {
