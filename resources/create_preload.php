c54891108fe411d94484bdc5249114f47efcce33
hugh.li <hugh.li@foxmail.com>
2024-01-25 15:52:50 +0800

    feat: 添加composer class to preload

--- resources/create_preload.php
+++ resources/create_preload.php
@@ -6,11 +6,20 @@ $classes = get_declared_classes();
 
 $publicPath = getcwd();
 
+/** web server */
 call_user_func(function () use ($publicPath) {
     ob_start();
     $_SERVER['REMOTE_ADDR'] = $_SERVER['REMOTE_ADDR'] ?? null ?: '127.0.0.1';
     require $publicPath.'/index.php';
     ob_clean();
+});
+
+/** composer classmap */
+call_user_func(function () use ($publicPath) {
+    $classmap = require $publicPath.'/../vendor/composer/autoload_classmap.php';
+    foreach ($classmap as $class => $file) {
+        class_exists($class);
+    }
 });
 
 $loads = Collection::make(get_declared_classes())
