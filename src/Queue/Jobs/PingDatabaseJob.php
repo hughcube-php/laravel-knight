577da7a6cf198f8210053c073d67eb64bf14a90a
hugh.li <hugh.li@foxmail.com>
2024-01-29 15:22:51 +0800

    feat: 心跳加上CONNECTION_ID

--- src/Queue/Jobs/PingDatabaseJob.php
+++ src/Queue/Jobs/PingDatabaseJob.php
@@ -4,7 +4,9 @@ namespace HughCube\Laravel\Knight\Queue\Jobs;
 
 use Exception;
 use HughCube\Laravel\Knight\Queue\Job;
-use HughCube\Laravel\Knight\Support\Carbon;
+use Illuminate\Database\Connection;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
 class PingDatabaseJob extends Job
@@ -23,22 +25,37 @@ class PingDatabaseJob extends Job
     {
         $connection = DB::connection($this->p('connection'));
 
-        $now = Carbon::now();
-        $connection->select('select 1', [], false);
-        $writePdoDuration = Carbon::now()->diffInMilliseconds($now);
+        $writeResultMessage = $this->pingResultMessage($connection, false);
 
-        $readPdoDuration = null;
+        $readResultMessage = null;
         if (!empty($connection->getConfig('read'))) {
-            $now = Carbon::now();
-            $connection->select('select 1');
-            $readPdoDuration = Carbon::now()->diffInMilliseconds($now);
+            $readResultMessage = $this->pingResultMessage($connection, false);
         }
 
         $this->info(sprintf(
-            'connection: %s, write: %s, read: %s, 心跳成功',
+            'connection: %s, write => %s, read => %s, 心跳成功',
             $connection->getName(),
-            sprintf('%sms', $writePdoDuration),
-            is_null($readPdoDuration) ? '-' : sprintf('%sms', $readPdoDuration)
+            $writeResultMessage,
+            ($readResultMessage ?? '-')
         ));
+    }
+
+    protected function prepareSql(Connection $connection): string
+    {
+        if ('mysql' === $connection->getDriverName()) {
+            return 'SELECT CONNECTION_ID()';
+        }
+        return 'SELECT 1';
+    }
+
+    protected function pingResultMessage(Connection $connection, $useReadPdo = true): string
+    {
+        $sql = $this->prepareSql($connection);
+
+        $now = Carbon::now();
+        $result = Collection::wrap((array) $connection->selectOne($sql, [], $useReadPdo))->first();
+        $duration = Carbon::now()->diffInMilliseconds($now);
+
+        return sprintf('(result: %s, duration: %s)', $result, $duration);
     }
 }
