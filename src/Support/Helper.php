d26143a2f98635cb07108149df78713f546513c8
hugh.li <hugh.li@foxmail.com>
2024-02-18 14:35:58 +0800

    feat: add Helper

---
+++ src/Support/Helper.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2024/2/18
+ * Time: 14:33
+ */
+
+namespace HughCube\Laravel\Knight\Support;
+
+use Exception;
+
+class Helper
+{
+    /**
+     * @throws Exception
+     */
+    public function assertClassExists(string $class)
+    {
+        if (!class_exists($class)) {
+            throw new Exception(sprintf("Class '%s' does not exist.", $class));
+        }
+    }
+}
