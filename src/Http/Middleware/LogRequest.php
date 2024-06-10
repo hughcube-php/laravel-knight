41fbb95f6f9ec1f4284814e071b38947b9e744f7
hugh.li <hugh.li@foxmail.com>
2024-06-10 11:43:48 +0800

    feat: add LogRequest

---
+++ src/Http/Middleware/LogRequest.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2021/10/28
+ * Time: 11:34.
+ */
+
+namespace HughCube\Laravel\Knight\Http\Middleware;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogRequest
+{
+    public function handle(Request $request, callable $next): Response
+    {
+        Log::info(
+            sprintf(
+                'request: uri:%s, headers:%s, body:%s',
+                $request->getUri(),
+                json_encode($request->headers->all()),
+                serialize($request->getContent())
+            )
+        );
+
+        return $next($request);
+    }
+}
