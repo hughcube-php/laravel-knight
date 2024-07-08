f156875a7359ab5b597170002da22ab8c84d9a1a
hugh.li <hugh.li@foxmail.com>
2024-07-08 10:06:19 +0800

    feat: add Middleware

---
+++ src/Http/Middleware/OnlyLocalEnvGuard.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2022/8/31
+ * Time: 18:21.
+ */
+
+namespace HughCube\Laravel\Knight\Http\Middleware;
+
+use Closure;
+use HughCube\Laravel\Knight\Traits\Container;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class OnlyLocalEnvGuard
+{
+    use Container;
+
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$this->isContainerLocalEnv()) {
+            throw new AccessDeniedHttpException('Only accessible in local env!');
+        }
+
+        return $next($request);
+    }
+}
---
+++ src/Http/Middleware/OnlyPublicIpGuard.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2022/8/31
+ * Time: 18:21.
+ */
+
+namespace HughCube\Laravel\Knight\Http\Middleware;
+
+use Closure;
+use HughCube\Laravel\Knight\Support\Str;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+
+class OnlyPublicIpGuard
+{
+    public function handle(Request $request, Closure $next)
+    {
+        if (!Str::isPublicIp($request->getClientIp())) {
+            throw new AccessDeniedHttpException('Only allow specified intranet ip access!');
+        }
+
+        return $next($request);
+    }
+}
