0bfa9767a59e91eb445f908863aebb90fc01ac2a
hugh.li <hugh.li@foxmail.com>
2025-04-14 22:13:31 +0800

    feat: base64_url

--- tests/Support/StrTest.php
+++ tests/Support/StrTest.php
@@ -23,4 +23,14 @@ class StrTest extends TestCase
         $this->assertSame(Str::countCommonChars('我喜欢编程', '程编让我快乐', true), 1);
         $this->assertSame(Str::countCommonChars('a喜欢编程我', '编a程让我快乐', true), 3);
     }
+
+    public function testBase64Url()
+    {
+        for ($i = 1; $i <= 100; $i++) {
+            $data = random_bytes($i * 100);
+
+            $string = Str::base64UrlEncode($data);
+            $this->assertSame($data, Str::base64UrlDecode($string));
+        }
+    }
 }
