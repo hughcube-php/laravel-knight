ad83c71eddeb7efde81bd399042b1ede226acf0a
hugh.li <hugh.li@foxmail.com>
2025-05-27 14:23:50 +0800

    feat: add GetUser interface

---
+++ src/Contracts/Support/GetUser.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: hugh.li
+ * Date: 2022/3/19
+ * Time: 17:57.
+ */
+
+namespace HughCube\Laravel\Knight\Contracts\Support;
+
+interface GetUser
+{
+    /**
+     * @return mixed
+     */
+    public function getUser();
+}
