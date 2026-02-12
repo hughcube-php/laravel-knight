fff81f652c2f98e5b15e6dd5bc59991d68a9340e
hugh.li <hugh.li@foxmail.com>
2026-02-12 16:39:42 +0800

    feat: Type 类新增 float、bool、collection 类型转换方法

--- src/Support/Type.php
+++ src/Support/Type.php
@@ -9,6 +9,8 @@
 
 namespace HughCube\Laravel\Knight\Support;
 
+use Illuminate\Support\Collection;
+
 class Type
 {
     public static function int($value): ?int
@@ -16,8 +18,23 @@ class Type
         return null === $value ? null : intval($value);
     }
 
+    public static function float($value): ?float
+    {
+        return null === $value ? null : floatval($value);
+    }
+
     public static function string($value): ?string
     {
         return null === $value ? null : strval($value);
+    }
+
+    public static function bool($value): ?bool
+    {
+        return null === $value ? null : boolval($value);
+    }
+
+    public static function collection($value): ?Collection
+    {
+        return null === $value ? null : Collection::make($value);
     }
 }
--- tests/Support/TypeTest.php
+++ tests/Support/TypeTest.php
@@ -4,6 +4,7 @@ namespace HughCube\Laravel\Knight\Tests\Support;
 
 use HughCube\Laravel\Knight\Support\Type;
 use HughCube\Laravel\Knight\Tests\TestCase;
+use Illuminate\Support\Collection;
 
 class TypeTest extends TestCase
 {
@@ -16,6 +17,15 @@ class TypeTest extends TestCase
         $this->assertSame(0, Type::int(0));
     }
 
+    public function testFloat()
+    {
+        $this->assertNull(Type::float(null));
+        $this->assertSame(1.0, Type::float(1));
+        $this->assertSame(1.5, Type::float('1.5'));
+        $this->assertSame(0.0, Type::float('0'));
+        $this->assertSame(0.0, Type::float(0));
+    }
+
     public function testString()
     {
         $this->assertNull(Type::string(null));
@@ -23,5 +33,22 @@ class TypeTest extends TestCase
         $this->assertSame('1', Type::string('1'));
         $this->assertSame('0', Type::string('0'));
         $this->assertSame('0', Type::string(0));
+    }
+
+    public function testBool()
+    {
+        $this->assertNull(Type::bool(null));
+        $this->assertSame(true, Type::bool(1));
+        $this->assertSame(false, Type::bool(0));
+        $this->assertSame(true, Type::bool('1'));
+        $this->assertSame(false, Type::bool(''));
+    }
+
+    public function testCollection()
+    {
+        $this->assertNull(Type::collection(null));
+        $this->assertInstanceOf(Collection::class, Type::collection([1, 2]));
+        $this->assertEquals([1, 2], Type::collection([1, 2])->all());
+        $this->assertInstanceOf(Collection::class, Type::collection('foo'));
     }
 }
